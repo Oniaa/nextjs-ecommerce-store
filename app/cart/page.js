c06f6812f07cbb205e9d1339057f6ef98ca3d512
ony <ogbuebele.ony@hotmail.com>
2023-05-26 13:10:48 +0200

    Add checkout button and thank you page

--- app/cart/page.js
+++ app/cart/page.js
@@ -1,6 +1,7 @@
 import { getProducts } from '../../database/products';
 import { getCookie } from '../../util/cookies';
 import { parseJson } from '../../util/json';
+import CheckOutButton from './CheckOutButton';
 import DeleteItemFromCart from './DeleteButton';
 
 export default async function CartPage() {
@@ -19,25 +20,42 @@ export default async function CartPage() {
     };
   });
 
+  const totalSum = carts.reduce((sum, item) => sum + parseInt(item.number), 0);
+
+  const totalPrice = orders.reduce(
+    (sum, item) => sum + parseInt(item.quantity) * parseInt(item.price),
+    0,
+  );
+
   console.log(orders);
 
   return (
     <main>
       <h1>Cart Page</h1>
       {orders.length > 0 && (
-        <div>
-          {orders.map((order) => {
-            return (
-              <div key={`cart-div-${order.id}`}>
-                <h2>{order.name}</h2>
-                <span>{order.price} $</span>
-                <br />
-                <span>{order.quantity}</span>
-                <DeleteItemFromCart productId={order.id} />
-              </div>
-            );
-          })}
-        </div>
+        <>
+          <div>
+            {orders.map((order) => {
+              return (
+                <div key={`cart-div-${order.id}`}>
+                  <h2>{order.name}</h2>
+                  <span>{order.price} $</span>
+                  <br />
+                  <span>{order.quantity}</span>
+                  <DeleteItemFromCart productId={order.id} />
+                </div>
+              );
+            })}
+          </div>
+          <br />
+          <br />
+          <div>
+            <span>
+              Total Quantity: {totalSum} Total Price: {totalPrice}$
+            </span>
+          </div>
+          <CheckOutButton />
+        </>
       )}
     </main>
   );
---
+++ app/thank-you/page.js
@@ -0,0 +1,11 @@
+export default function ThankYouPage() {
+  return (
+    <main>
+      <h1>
+        Thank you for your order!
+        <br />
+        And have fun with your purchase
+      </h1>
+    </main>
+  );
+}
