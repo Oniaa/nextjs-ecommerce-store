31a7427caf53ded1449a79159e6a34d3c2540440
ony <ogbuebele.ony@hotmail.com>
2023-05-26 16:28:40 +0200

    Add styling to products page

--- app/products/page.js
+++ app/products/page.js
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import { getProducts } from '../../database/products';
+import style from './page.module.scss';
 
 export default async function ProductsPage() {
   const products = await getProducts();
@@ -8,21 +9,28 @@ export default async function ProductsPage() {
   console.log(products);
 
   return (
-    <main>
-      <h1>Bubbles</h1>
-      {products.map((product) => {
-        return (
-          <div key={`product-div-${product.id}`}>
-            <Link href={`/products/${product.id}`}>{product.name}</Link>
-            <Image
-              src={`/images/${product.name}.png`}
-              width={200}
-              height={200}
-              alt="Bubbles"
-            />
-          </div>
-        );
-      })}
+    <main className={style.mainContainer}>
+      <h1>All Bubbles</h1>
+      <div className={style.productContainer}>
+        {products.map((product) => {
+          return (
+            <div
+              className={style.productContent}
+              key={`product-div-${product.id}`}
+            >
+              <Image
+                src={`/images/${product.name}.png`}
+                width={200}
+                height={200}
+                alt="Bubbles"
+              />
+              <Link href={`/products/${product.id}`}>
+                {product.name} {product.price}$
+              </Link>
+            </div>
+          );
+        })}
+      </div>
     </main>
   );
 }
