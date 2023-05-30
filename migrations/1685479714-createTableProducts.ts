0821f74571bb9cfcbd23153c12d6bd9df158ec1c
ony <ogbuebele.ony@hotmail.com>
2023-05-30 23:09:19 +0200

    Create new table with product descrition etc

---
+++ migrations/1685479714-createTableProducts.ts
@@ -0,0 +1,29 @@
+import { Sql } from 'postgres';
+
+export type Product = {
+  id: number;
+  name: string;
+  price: number;
+  productName: string;
+  description: string;
+  shortDescription: string;
+};
+
+export async function up(sql: Sql) {
+  await sql`
+    CREATE TABLE products (
+      id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+      name varchar(30) NOT NULL,
+      price integer NOT NULL,
+      product_Name varchar(100) NOT NULL,
+      description varchar(10000) NOT NULL,
+      short_Description varchar(1000) NOT NULL
+    )
+  `;
+}
+
+export async function down(sql: Sql) {
+  await sql`
+    DROP TABLE products
+  `;
+}
